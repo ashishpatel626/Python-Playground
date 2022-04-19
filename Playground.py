b9c82c1c2111bd3a73771b7a48b26f4f61591e45
Ashish Patel <ashishpatel626@lavabit.com>
2022-04-18 21:42:09 -0400

    OOP practice

--- Playground.py
+++ Playground.py
@@ -1,12 +1,5 @@
-def simpleGeneratorFun():
-    yield 1
-    yield 2
-    yield 3
-   
-# x is a generator object
-x = simpleGeneratorFun()
-  
-# Iterating over the generator object using next
-print(x.__next__()) # In Python 3, __next__()
-print(x.__next__())
-print(x.__next__())
+l = [[1,2,3],[4,5,6],[7,8,9]]
+
+
+
+print(l.size())
