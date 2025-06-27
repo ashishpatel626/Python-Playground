b5101072c82be5950666767a8c818c181d2f13c0
Ashish Patel <ashishpatel626@gmail.com>
2025-06-26 20:11:19 -0400

    Add Pet and Student classes with multiple inheritance
    Introduced a Pet class and a Student class that inherits from both Person and Pet, demonstrating multiple inheritance. Updated Person's method and constructor for compatibility, and added example usage in the main block.

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -1,13 +1,33 @@
 class Person:
-    def __init__ (self, firstName, lastName):
+    def __init__(self, firstName, lastName, **kwargs):
+        super().__init__(**kwargs)
         self.firstName = firstName
         self.lastName = lastName
 
-    def printFullName(self):
+    def fullName(self):
         fullName = self.firstName + " " + self.lastName
         return fullName
-    
+
+class Pet:
+    def __init__(self, animal, age, color, **kwargs):
+        super().__init__(**kwargs)
+        self.animal = animal
+        self.age = age
+        self.color = color
+
+    def petInfo(self):
+        info = f'Type: {self.animal}, Age: {self.age}, Color: {self.color}'
+        return info
+
+class Student(Person, Pet):
+    def __init__(self, firstName, lastName, animal, age, color, id, dorm):
+        super().__init__(firstName=firstName, lastName=lastName, animal=animal, age=age, color=color)
+        self.id = id
+        self.dorm = dorm
 
 if __name__ == '__main__':
     Name = Person("Solid", "Snake")
-    print(Name.printFullName())
+    student1 = Student('Raiden', 'El', 'tiger', 20, 'black', 101, 'South Building')
+
+    print(student1.petInfo())
+    
