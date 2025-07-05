db6a49df752f3cd493befc8aac2f609dd6635c79
Ashish Patel <ashishpatel626@gmail.com>
2025-07-05 12:15:59 -0400

    Refactor OOP examples and update Playground notes
    Removed Encapsulation.py, added multipledispatch usage and new class 'edurance' in Inheritance.py, and updated titan class with method overloading. Playground.py now contains explanatory notes on tuples and lists, and a placeholder function for string processing.

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -1,3 +1,5 @@
+from multipledispatch import dispatch
+
 class hardening:
     def __init__(self, thickness: int, color: str, **kwargs) -> None:
         self.thickness = thickness
@@ -13,9 +15,17 @@ class stamina:
     def jump(self) -> str:
         return 'Can Jump'
     
+    
     def fly(self) -> str:
         return 'Can fly'
-        
+
+class edurance:
+    def _init_(self, speed: int, jump_height: int):
+        self.speed = speed
+        self.jump_height = jump_height
+    
+    def info(self):
+        return f'Speed: {self.speed} Jump Height: {self.jump_height}'
 
 class titan(hardening, stamina):
     def __init__(self, name: str, ability: str, height: int, thickness: int, color: str, transform_times: int, duration: int, **kwargs) -> None:
@@ -27,17 +37,19 @@ class titan(hardening, stamina):
     def info(self) -> str:
         info = f'Name: {self.name} | Ability: {self.ability} | Height: {self.height} | Thickness: {self.thickness} | Color: {self.color} | Number of Transformations: {self.transform_times} | Duration {self.duration}'
         return info
-
-    def fly(self) -> str:
-        return 'Can only Glide'
     
-
+    @dispatch(int)
+    def fly(self, dive: int) -> str:
+        self.dive = dive
+        return f'Can only Glide {self.dive}'
 
 if __name__ == '__main__':
     armored_titan = titan('Armored Titan', 'Covered in thick hardening', 50, 5, 'black', 3, 5)
 
     print(armored_titan.info())
-    print(armored_titan.fly())
+    print(armored_titan.fly(6))
+    print(armored_titan.info())
+
 
 
     
--- Playground.py
+++ Playground.py
@@ -1,5 +1,19 @@
-if __name__ == '__main__':
-    n1 = 'dogs'
-    n2 = 'cats'
-    string = (f'Its raining {n1} and {n2}')
-    print(string)
+'''
+A tuple is non-mutable, random access, ordered and allows duplicates.
+A list is ordered, has random access, mutable and allows duplicates.
+
+use a tuple when you have a static dataset. Both have a random access of o(1)
+
+odd_num = [x for x in range(10) if x % 2 != 0]
+
+def odd_num_generator():
+    y = 0
+    while y <= 10:
+        if y % 2 != 0:
+            yield y
+        y += 1
+
+
+'''
+def string(string: list[str] ) -> list[str]:
+    pass
