6e55f184a1e5334d11d95af62f2185b091c90f71
Ashish Patel <ashishpatel626@gmail.com>
2025-07-10 22:32:35 -0400

    Add addition class and update Playground.py
    Introduced an addition class with overloaded __add__ methods using singledispatchmethod. Also added PyCharm project configuration files to the repository.

--- Playground.py
+++ Playground.py
@@ -1,8 +1,19 @@
-'''
-A list is mutable, ordered, has random access and can contain duplicates.
-The only difference in a tuple is that it is non mutable and both have a random access of o(1).
-You would use a tuple when you have a static dataset.
-'''
+from functools import singledispatchmethod
+
+
+class addition:
+    def __init__(self, a: int, b:int, z:int) -> None:
+        self.a = a
+        self.b = b
+        self.z = z
+
+    @singledispatchmethod
+    def __add__(self, other) -> int:
+        return addition(self.a + other.a, self.b + other)
+
+    @__add__.register(int)
+    def __add__(self, other) -> int:
+        return addition(self.a + other.a, self.b, self.z + other)
 
 odd_list = [x for x in range(10) if x%2 != 0]
 
@@ -14,10 +25,12 @@ def filter_string(strings: list[str], min_length: int) -> list[str]:
     string_list = [x for x in strings if len(x) >= min_length]
     return string_list
 
- 
+
 
 
 if __name__ == '__main__':
+    addition1 = addition(2, 4, 5)
+    addition1.__add__
     # print(odd_list)
     # print(list(odd_num_generator()))
     # print(filter_string(['test1', 'test2', 'test3', 'test4', 'te'], 3))
